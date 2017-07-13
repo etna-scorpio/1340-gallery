2d10a1720a25f8bb20541a508ed5e8bcd95f18c8
Etna <etna.scorpio@gmail.com>
2017-07-13 19:00:23 +0300

    Add two side images for big screens

---
+++ build/js/_detectIE.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  var trident = ua.indexOf('Trident/');
+  var edge = ua.indexOf('Edge/');
+  if (msie > 0 || trident > 0 || edge > 0) {
+    $('body').addClass('is-ie');
+  }
+
+  // other browser
+  return false;
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,13 @@
 // example of simple includes for js
 //=include lib/jquery.min.js
-//=include _context-menu.js
+//=include lib/object-fit.min.js
+//=include _detectIE.js
+
+
+$(document).ready(function() {
+	//=include _context-menu.js
+	detectIE();
+
+	$(function () { objectFitImages('.js-fit-img') });
+});
 
